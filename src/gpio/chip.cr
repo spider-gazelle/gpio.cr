9263e80359f900c3243ff1cb7ca70cdda4c07b21
Stephen von Takach <steve@place.technology>
2025-01-08 17:26:46 +1100

    fix: GC warnings

--- src/gpio/chip.cr
+++ src/gpio/chip.cr
@@ -1,10 +1,22 @@
 class GPIO::Chip
+  class Ref
+    def initialize(@to_unsafe : LibGPIOD::Chip)
+    end
+
+    getter to_unsafe : LibGPIOD::Chip
+
+    def finalize
+      LibGPIOD.chip_close @to_unsafe
+    end
+  end
+
   def self.all
     Dir.glob("/dev/gpiochip*").sort!.map { |path| new(Path[path]) }
   end
 
   def initialize(path : Path)
     @chip = LibGPIOD.chip_open(path.to_s)
+    @ref = Ref.new(@chip)
     raise "failed to open chip @ #{path}" if @chip.null?
   end
 
@@ -16,10 +28,6 @@ class GPIO::Chip
 
   def to_unsafe
     @chip
-  end
-
-  def finalize
-    LibGPIOD.chip_close @chip
   end
 
   def to_s(io : IO)
